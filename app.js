583817c2e100de6415910b8abe166bb59b47af31
HyoSung <hyosung11@me.com>
2019-09-12 11:47:59 -0400

    angular setup

--- app.js
+++ app.js
@@ -5,5 +5,68 @@ const app = angular.module('RefereeApp', [])
 
 // app.controller is the controller for all functions
 app.controller('MainController', function() {
-  this.hello = 'started this app'
+  this.hello = 'Welcome to the Referee Fees Tracker'
+
+  this.games = [
+    {
+      date: '20190909',
+      time: '19:00',
+      location: 'Westwood High School',
+      competition: 'Big North - BCSOA',
+      home: 'Westwood',
+      away: 'River Dell',
+      fee: 80,
+      payment: 'check',
+      status: 'pending',
+      note: 'worked with Scott Riker'
+    },
+    {
+      date: '20190909',
+      time: '19:00',
+      location: 'Westwood High School',
+      competition: 'Big North - BCSOA',
+      home: 'Westwood',
+      away: 'River Dell',
+      fee: 80,
+      payment: 'check',
+      status: 'pending',
+      note: 'worked with Scott Riker'
+    },
+    {
+      date: '20190909',
+      time: '19:00',
+      location: 'Westwood High School',
+      competition: 'Big North - BCSOA',
+      home: 'Westwood',
+      away: 'River Dell',
+      fee: 80,
+      payment: 'check',
+      status: 'pending',
+      note: 'worked with Scott Riker'
+    },
+    {
+      date: '20190909',
+      time: '19:00',
+      location: 'Westwood High School',
+      competition: 'Big North - BCSOA',
+      home: 'Westwood',
+      away: 'River Dell',
+      fee: 80,
+      payment: 'check',
+      status: 'pending',
+      note: 'worked with Scott Riker'
+    },
+    {
+      date: '20190909',
+      time: '19:00',
+      location: 'Westwood High School',
+      competition: 'Big North - BCSOA',
+      home: 'Westwood',
+      away: 'River Dell',
+      fee: 80,
+      payment: 'check',
+      status: 'pending',
+      note: 'worked with Scott Riker'
+    }
+  ];
 })
